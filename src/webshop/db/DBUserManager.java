454e1946178e21825a05a62dfafc5d69b0b9b39e
Erik Lindfors <elindfo@kth.se>
2018-10-02 00:21:31 +0200

    Add method for user authentication validation

--- src/webshop/db/DBUserManager.java
+++ src/webshop/db/DBUserManager.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 //https://stackoverflow.com/questions/10006165/converting-string-to-character-array-in-java/10006225
@@ -19,27 +20,65 @@ public class DBUserManager {
     }
 
     public boolean authenticate(String username, String password){
-        return true;
+
+        try {
+            String encryptedPassword = hashWithSHA256(password);
+
+            dbManager.getConnection().setAutoCommit(false);
+
+            String query = "SELECT count(*) FROM user WHERE hashedpw = ? AND uname = ?";
+
+            PreparedStatement psmt = dbManager.getConnection().prepareStatement(query);
+
+            psmt.setString(1, encryptedPassword);
+            psmt.setString(2, username);
+
+            ResultSet resultSet = psmt.executeQuery();
+
+            boolean userFound = false;
+            if(resultSet.next()){
+                userFound = resultSet.getInt(1) != 0;
+            }
+            dbManager.getConnection().commit();
+            return userFound;
+        } catch (SQLException e) {
+            System.out.println("SQLException");
+            System.out.println("Error: " + e.getErrorCode());
+            if(dbManager.getConnection() != null){
+                try {
+                    dbManager.getConnection().rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            return false;
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Exception: Unable to find hashing algorithm");
+            return false;
+        } finally{
+            if(dbManager.getConnection() != null){
+                try{
+                    dbManager.getConnection().setAutoCommit(true);
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public boolean addUser(String username, String password){
         try{
-
             String encryptedPassword = hashWithSHA256(password);
-            System.out.println("Password: " + password);
-            System.out.println("Encrypted: " + encryptedPassword);
 
             dbManager.getConnection().setAutoCommit(false); //Initiate transaction
 
-            StringBuffer addUserString = new StringBuffer();
-            addUserString.append("INSERT INTO user (uname, hashedpw) ");
-            addUserString.append("VALUES (?, ?);");
+            String query = "INSERT INTO user (uname, hashedpw) VALUES (?, ?);";
 
-            PreparedStatement smt = dbManager.getConnection().prepareStatement(addUserString.toString());
+            PreparedStatement psmt = dbManager.getConnection().prepareStatement(query);
 
-            smt.setString(1, username);
-            smt.setString(2, encryptedPassword.toString());
-            smt.execute();
+            psmt.setString(1, username);
+            psmt.setString(2, encryptedPassword);
+            psmt.execute();
             dbManager.getConnection().commit();
             return true;
         } catch (SQLException e) {
@@ -80,9 +119,11 @@ public class DBUserManager {
 
     public static void main(String[] args) {
         DBUserManager dbUserManager = new DBUserManager();
-        dbUserManager.addUser("erik", "hejsan");
+        /*dbUserManager.addUser("erik", "hejsan");
         dbUserManager.addUser("joacim", "tjenare");
         dbUserManager.addUser("joacima", "tjenare");
         dbUserManager.addUser("ecke", "apa");
+        dbUserManager.addUser("eckea", "adspa");*/
+        System.out.println("Authenticated: " + dbUserManager.authenticate("ecke", "apa"));
     }
 }
