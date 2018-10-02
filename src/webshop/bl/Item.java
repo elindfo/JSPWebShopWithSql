66ca4ab99d3bb7ceec12b0c59d132e1fe2f302d2
Erik Lindfors <elindfo@kth.se>
2018-10-03 01:59:42 +0200

    Implement Item class and category enum

--- src/webshop/bl/Item.java
+++ src/webshop/bl/Item.java
@@ -1,4 +1,38 @@
 package webshop.bl;
 
 public class Item {
+
+    public enum category{
+        SPORTS, ELECTRONICS, MUSIC
+    }
+
+    private String name;
+    private double price;
+    private int qty;
+
+    public Item(String name, double price, int qty){
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 }
