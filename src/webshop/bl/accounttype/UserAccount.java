9570b4980d886b05c0a03b09c8a9a897c6f706d5
Erik Lindfors <elindfo@kth.se>
2018-10-06 13:23:33 +0200

    Implement basic UserAccount logic

--- src/webshop/bl/accounttype/UserAccount.java
+++ src/webshop/bl/accounttype/UserAccount.java
@@ -1,9 +1,29 @@
 package webshop.bl.accounttype;
 
-public class UserAccount extends Account{
+import webshop.bl.Item;
+import webshop.bl.ShoppingCart;
+
+import java.util.List;
+
+public class UserAccount extends Account {
+
+    private ShoppingCart shoppingCart;
 
     public UserAccount(int uid) {
         super(uid);
+        shoppingCart = new ShoppingCart();
+    }
+
+    public boolean addItemToShoppingCart(Item item){
+        return shoppingCart.add(item);
+    }
+
+    public boolean removeItemFromShoppingCart(Item item){
+        return shoppingCart.remove(item);
+    }
+
+    public List<Item> viewShoppingCart(){
+        return shoppingCart.getItems();
     }
 
     @Override
