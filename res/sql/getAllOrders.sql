ad756071974beab7f462689ff8a609c9b9a0f3d9
Jocke <joacimgr@kth.se>
2018-10-08 14:10:28 +0200

    Add script getAllOrders

---
+++ res/sql/getAllOrders.sql
@@ -0,0 +1,8 @@
+SELECT oid FROM user_order;
+
+SELECT order_item.iid, item.name, order_item.iqty, item_category.category, item_prc.prc
+FROM order_item
+JOIN item ON order_item.iid = item.iid
+JOIN item_category ON order_item.iid = item_category.iid
+JOIN item_prc ON order_item.iid = item_prc.iid
+WHERE order_item.oid = 1
