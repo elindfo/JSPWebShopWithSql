e589b6e2aba9ab039b8f056ea4dd326af633899a
Erik Lindfors <elindfo@kth.se>
2018-10-08 17:20:54 +0200

    Update getAllOrders.sql to include packed status

--- res/sql/getAllOrders.sql
+++ res/sql/getAllOrders.sql
@@ -1,8 +1,9 @@
 SELECT oid FROM user_order;
 
-SELECT order_item.oid, order_item.iid, item.iname, order_item.iqty, item_category.category, item_prc.prc
+SELECT order_item.oid, order_item.iid, item.iname, order_item.iqty, item_category.category, item_prc.prc, user_order.packed
 FROM order_item
 JOIN item ON order_item.iid = item.iid
 JOIN item_category ON order_item.iid = item_category.iid
 JOIN item_prc ON order_item.iid = item_prc.iid
-WHERE order_item.oid = 1
+JOIN user_order ON order_item.oid = user_order.oid
+WHERE order_item.oid = 1 OR order_item.oid = 2
